b25b4458c440c72d074804deea51cfc6ef813727
Amilcar Velez <44828356+amilkant@users.noreply.github.com>
2023-07-09 17:21:57 -0500

    Update MomentFunct_Uv2.m

--- matlab/1_functions/MomentFunct_Uv2.m
+++ matlab/1_functions/MomentFunct_Uv2.m
@@ -1,7 +1,7 @@
 % this function compute a moment function for an alternative specification for e_ij (see Assumption 3.2)
 %
 % comment:
-% - the moment function utilized in this function is an alternative to eq (27).
+% - the moment function utilized in this function is an alternative to eq (28).
 %   It can be derived following the derivations presented in Section 4.1
 %   and assuming e_ij = g(theta,O_i) + V_ij instead of  Assumption 3.2
 
@@ -40,7 +40,7 @@ function salida = MomentFunct_Uv2(A_vec, D_vec, Dist_vec, Z_vec, J0_vec, theta, 
 
             g_theta = theta_jj0 + theta_jj1 * Dist_vec(jj0) + theta_jj2 * Dist_vec(jj0) ^ 2; % alternative specification as in Section 8.2.3
 
-            salida(jj0) = ((A_vec(jj0) + g_theta) .* D_vec(jj0) - Vbar * (1 - D_vec(jj0))) .* Z_vec(jj0); % use g_theta instead of theta, see eq (26) and Section 8.2.3
+            salida(jj0) = ((A_vec(jj0) + g_theta) .* D_vec(jj0) - Vbar * (1 - D_vec(jj0))) .* Z_vec(jj0); % use g_theta instead of theta, see eq (28) and Section 8.2.3
         end
 
     end
